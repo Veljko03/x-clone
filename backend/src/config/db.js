7296efacf12c24f9db30f07d9e6c12523aaa89f3
veljko03 <veljkodovragovic@gmail.com>
2025-10-17 08:00:11 +0200

    updated ndoe version

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,19 +2,11 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-  if (!ENV.MONGO_URI) {
-    console.error("MONGO_URI is not defined!");
-    return; // ne izlazi sa process.exit() u serverless okruženju
-  }
-
   try {
-    await mongoose.connect(ENV.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(ENV.MONGO_URI);
     console.log("Connected to DB SUCCESSFULLY ✅");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    // Ne radi process.exit() u serverless, samo loguj grešku
+    console.log("Error connecting to MONGODB");
+    process.exit(1);
   }
 };
