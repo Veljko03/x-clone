34e9cd4ea1a1181af7208cb98e7b37cf7f187b94
veljko03 <veljkodovragovic@gmail.com>
2025-10-17 07:41:52 +0200

    checked url mongo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async () => {
+  if (!ENV.MONGO_URI) {
+    console.error("MONGO_URI is not defined!");
+    return; // ne izlazi sa process.exit() u serverless okruženju
+  }
+
   try {
-    await mongoose.connect(ENV.MONGO_URI);
+    await mongoose.connect(ENV.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to DB SUCCESSFULLY ✅");
   } catch (error) {
-    console.log("Error connecting to MONGODB");
-    process.exit(1);
+    console.error("Error connecting to MongoDB:", error.message);
+    // Ne radi process.exit() u serverless, samo loguj grešku
   }
 };
