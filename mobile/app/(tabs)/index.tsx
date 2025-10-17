20dc492a9fdb4b51211114fa9869ccbfcd5d9543
veljko03 <veljkodovragovic@gmail.com>
2025-10-17 05:01:03 +0200

    handled logging out

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
+import SignOutButton from "@/components/SignOutButton ";
 import { View, Text } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 const HomeScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>ssssssssssffffffff</Text>
-    </View>
+      <SignOutButton />
+    </SafeAreaView>
   );
 };
 export default HomeScreen;
